a0ef17be3f5f457cf205633a839194c6964e7135
OliviaPiwe <olivia_piechowski@hotmail.de>
2024-10-17 16:20:57 +0200

    better

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 export function Main() {
   return (
     <main className="main">
-      <h1>Cans Business coming soon</h1>
+      <h1>Cans Business</h1>
       <h3>work in progress..</h3>
     </main>
   );
